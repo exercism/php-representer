c621eaadccdd7921f3a0ba04323f7e4936872ada
homersimpsons <guillaume.alabre@gmail.com>
2023-03-26 01:21:30 +0100

    :sparkles: Normalize internal method aliases

--- src/Mapping.php
+++ src/Mapping.php
@@ -55,13 +55,46 @@ class Mapping
         return json_encode($mapping, JSON_FORCE_OBJECT | JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE);
     }
 
+    /**
+     * TRANSFORM: normalize PHP's aliased function names (only Base syntax)
+     *
+     * @see https://www.php.net/manual/en/aliases.php
+     */
+    private function functionAlias(string $name): string
+    {
+        return match ($name) {
+            'chop' => 'rtrim',
+            // 'close' => 'closedir', // close() does not exist in PHP 8.0 (no documentation)
+            'doubleval' => 'floatval',
+            'fputs' => 'fwrite',
+            'ini_alter' => 'ini_set',
+            'is_double' => 'is_float',
+            'is_integer' => 'is_int',
+            'is_long' => 'is_int',
+            // 'is_real' => 'is_float', // is_real() is removed in PHP 8.0
+            'is_writeable' => 'is_writable',
+            'join' => 'implode',
+            'key_exists' => 'array_key_exists',
+            'pos' => 'current',
+            'show_source' => 'highlight_file',
+            'sizeof' => 'count',
+            'strchr' => 'strstr',
+            default => $name,
+        };
+    }
+
     public function addFunction(string $name): string
     {
         // TRANSFORM: Function names are case-insensitive in PHP
         $name = mb_strtolower($name);
-        // Do not rename built-in functions, functions_exists() includes user-defined functions
+        // Do not rename built-in functions except aliased ones, functions_exists() includes user-defined functions
         if (array_key_exists($name, $this->internalFunctions)) {
-            return $name;
+            $unaliasedName = $this->functionAlias($name);
+            if ($unaliasedName !== $name) {
+                $this->invertedFunctionMapping[$name] = $unaliasedName;
+            }
+
+            return $unaliasedName;
         }
 
         if (! isset($this->invertedFunctionMapping[$name])) {
